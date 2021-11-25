cb8e51b024b0ea66581a474c4bfed337b4f65a72
agora <adrian.gora14@gmail.com>
2021-11-25 22:04:13 +0100

    adding ToEthAddress component

---
+++ src/components/ToEthAddress/ToEthAddress.jsx
@@ -0,0 +1,17 @@
+import { Godwoker } from "@polyjuice-provider/base";
+
+export const ToEthAddress = async (polyjuiceAddress) => {
+  const godwokenRPC = 'https://godwoken-testnet-web3-rpc.ckbapp.dev';
+
+  const godwoker = new Godwoker(godwokenRPC, {
+    godwoken: {
+      rollup_type_hash: '0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a',
+      eth_account_lock: {
+        code_hash: '0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22',
+        hash_type: 'type'
+      }
+    }
+  });
+
+  return await godwoker.getEthAddressByAllTypeShortAddress(polyjuiceAddress);
+};
