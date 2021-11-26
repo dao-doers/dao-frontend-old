95c5887242648b871ec10a44463f092d32336594
agora <adrian.gora14@gmail.com>
2021-11-26 21:22:27 +0100

    fixed NaN

--- src/components/Token/Token.jsx
+++ src/components/Token/Token.jsx
@@ -21,9 +21,18 @@ const _check404 = (image_url) => {
   }
 }
 
+const NanValue = function (entry) {
+  if(entry === "NaN") {
+      return 0.00;
+  } else {
+      return entry;
+  }
+}
+
 const _getBalanceLabel = (quantity, decimals) => {
   const zeroes = (!decimals) ? 0 : Number(decimals);
-  return numeral(new BigNumber(quantity).dividedBy(Math.pow(10, zeroes)).toNumber()).format('0,0.[00]');
+
+  return NanValue(numeral(new BigNumber(quantity).dividedBy(Math.pow(10, zeroes)).toNumber()).format('0,0.[00]'));
 }
 
 /**
